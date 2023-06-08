b918ee55fc6077fda8953bdfbc1d5bf1f91dfa63
NiezTsuki <danieljofremolina@gmail.com>
2023-06-08 00:15:12 -0400

    Se avanza en el proyecto

--- gymbros/settings.py
+++ gymbros/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'gymbros.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'Tienda.db',
     }
 }
 
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-cl'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Santiago'
 
 USE_I18N = True
 
--- tienda/models.py
+++ tienda/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class producto(models.Model):
+    id     =models.CharField(primary_key=True, max_length=10)
+    nombre =models.CharField(max_length=20)
+    desc   =models.CharField(max_length=200)
+    precio =models.CharField(max_length=10)
+
