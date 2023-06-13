a2b8a00df82ccca3afe280c41b516b948bb0f529
NiezTsuki <danieljofremolina@gmail.com>
2023-06-12 23:48:38 -0400

    Se agrego imagenes a traves de la base de datos

--- tienda/models.py
+++ tienda/models.py
@@ -2,9 +2,28 @@ from django.db import models
 
 # Create your models here.
 
-class producto(models.Model):
-    id     =models.CharField(primary_key=True, max_length=10)
-    nombre =models.CharField(max_length=20)
-    desc   =models.CharField(max_length=200)
-    precio =models.CharField(max_length=10)
+class Categoria(models.Model):
+    id_categoria     =models.AutoField(db_column='idCategoria', primary_key=True)
+    categoria        =models.CharField(max_length=20, blank=False, null=False)
+    
 
+    def __str__(self):
+        return str(self.categoria)
+
+
+class Producto(models.Model):
+
+    id_producto     = models.CharField(primary_key=True, max_length=10)
+    nombre           = models.CharField(max_length=20)
+    desc             = models.CharField(max_length=250)
+    precio           = models.CharField(max_length=10)
+    id_categoria     = models.ForeignKey(Categoria,on_delete=models.CASCADE, db_column='idCategoria')  
+    imagen           = models.ImageField(upload_to="productos", null=True)
+    activo           = models.IntegerField()
+
+
+    def __str__(self):
+        texto = "{0} ({1})"
+        return texto.format(self.nombre, self.desc, self.precio, self.imagen)
+
+    
--- tienda/views.py
+++ tienda/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Producto
 
 # Create your views here.
 def index(request):
     return render(request,'tienda/index.html')
+
+def productos(request):
+    producto = Producto.objects.all()
+    return render(request, 'tienda/productos.html', {'productos' : producto})
+    
